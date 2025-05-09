77b0d4c867e8c71d0d58c1cdf636c0bbb503b861
NClouser1 <100168383+NClouser1@users.noreply.github.com>
2025-05-09 07:40:03 -0400

    Updated run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,4 @@
 
 # Start the Othello server
 echo "Starting Othello server..."
-javac -cp AI/lib/gson-2.8.5.jar -d AI/bin AI/src/com/atomicobject/othello/*.java
 java -jar othello.jar --p1-type remote --p2-type random --wait-for-ui
